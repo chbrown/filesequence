37e0b4a5c880a8e7d1ed20e4cb20d6592aa16285
Christopher Brown <io@henrian.com>
2013-08-01 15:55:09 -0500

    Make PyPI happier

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 from setuptools import setup
-# import os
-# import sys
 
 setup(
     name='filesequence',
     version='0.1.1',
+    description='Python HTTP for Humans.',
+    long_description=open('README.md').read(),
+    license=open('LICENSE').read(),
+    author='Christopher Brown',
+    author_email='io@henrian.com',
+    url='https://github.com/chbrown/filesequence',
     packages=['filesequence'],
+    package_dir={
+        'filesequence': 'filesequence'
+    },
+    package_data={
+        None: ['LICENSE']
+    },
+    include_package_data=True,
     install_requires=[],
     entry_points={
         'console_scripts': [
             'filesequence = filesequence:main',
         ],
     },
-    test_suite='tests',
+    zip_safe=True,
 )
