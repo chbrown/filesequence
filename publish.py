fa9b595721b8bc019422b08e2fa4a704011f7e36
Christopher Brown <io@henrian.com>
2013-08-16 14:50:06 -0500

    Don't try to retag after 'npm version' call

--- publish.py
+++ publish.py
@@ -17,30 +17,28 @@ print "1. Convert latest README Markdown to reStructuredText, because PyPI can't
 
 call('pandoc', 'README.md', '-o', 'README.rst')
 
-print '2. Bump version in package.json. If you have any uncommitted changes,'
-print '   this will exit immediately. Commit and then re-run this file.'
+print '2. Bump version in package.json and create tag. If you have any uncommitted changes, this will exit immediately. Commit and then re-run this file.'
 
 call('npm', 'version', 'patch')
+
+# alternatively, manually set the new version number and then run:
+# import filesequence
+# call('git', 'tag', '-a', 'v' + filesequence.__version__, '-m', filesequence.__version__)
 
 print '3. Reinstall as the new version'
 
 call('python', 'setup.py', 'develop')
 
-print '4. Tag latest'
-
-import filesequence
-call('git', 'tag', '-a', 'v' + filesequence.__version__, '-m', filesequence.__version__)
-
-print '5. Push (Github will automatically prepare the static version, if needed)'
+print '4. Push (Github will automatically prepare the static version, if needed)'
 
 call('git', 'push')
 
-print '6. Publish (will be on PyPI, but not every setuptools incarnation will be able to get to it)'
+print '5. Publish (will be on PyPI, but not every setuptools incarnation will be able to get to it)'
 
 call('python', 'setup.py', 'register')
 
-print '7. Upload source distribution (which allows some of the older distutils / setuptools distribution to install it)'
+print '6. Upload source distribution (which allows some of the older distutils / setuptools distribution to install it)'
 
 call('python', 'setup.py', 'sdist', 'upload')
 
-print '8. Check PyPI simple at https://pypi.python.org/simple/filesequence/ to ensure this package was uploaded'
+print '7. Check PyPI simple at https://pypi.python.org/simple/filesequence/ to ensure this package was uploaded'
